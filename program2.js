a195d76283512d154cd58b6af2670c58312bf6cb
Andrey <andrei19386@mail.ru>
2023-04-16 17:12:28 +0300

    Homework 4 is done!

---
+++ program2.js
@@ -0,0 +1,16 @@
+//Задание 1
+
+ let a = + prompt("Input a");
+ let b = + prompt("Input b");
+ let message = a + b > 15 ? "ДА" : "НЕТ";
+ console.log(message);
+
+ //Задание 2
+
+let c = '15';
+let d = prompt("Input the second variable");
+if(c === d){
+    console.log("The variables are equivalent, true!");
+} else {
+    console.log("False, they are not equivalent");
+}
