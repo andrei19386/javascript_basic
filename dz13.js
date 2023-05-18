b9b4035fb58de2bea61f204a53ce9b237886a660
Andrey <andrei19386@mail.ru>
2023-05-18 12:48:02 +0300

    Homework13 is partially done

---
+++ dz13.js
@@ -0,0 +1,39 @@
+// let a = [1,2,3];
+// console.log(typeof a);
+
+
+// let btn = document.querySelector('.qwew');
+// btn.onclick = ()=>{
+//     localStorage.setItem('data',JSON.stringify(a));
+//     document.querySelector('.qwer').innerHTML = localStorage.getItem('data');
+//     let b = localStorage.getItem('data');
+//     console.log(b);
+//     console.log(typeof b);
+//     b = JSON.parse(b);
+//     console.log(b);
+//     console.log(typeof b);
+//     console.log(b[0]);
+//     console.log(typeof b);
+// }
+
+ 
+
+let xhttp = new XMLHttpRequest();
+
+
+xhttp.onreadystatechange = function(){
+    if(this.readyState == 4 && this.status == 200){
+        myfunc(this.responseText);
+    }
+}
+
+//xhttp.open('GET','http://127.0.0.1:8080/api/v1/geo/countries/',true);
+
+xhttp.open('GET','http://127.0.0.1:8081',true);
+
+
+xhttp.send();
+
+function myfunc(data){
+    console.log(data);
+}
