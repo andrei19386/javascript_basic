382df1df8de26f741ff34e1337d83c5833f7eaae
Andrey <andrei19386@mail.ru>
2023-06-08 14:31:33 +0300

    First version of web

---
+++ encycl/js/main.js
@@ -0,0 +1,11 @@
+const button1 = document.querySelector(".v1_238");
+console.log(button1);
+button1.onclick = () => {
+    alert('Функция пока в разработке!')
+}
+
+const button2 = document.querySelector(".v1_222"); 
+console.log(button2);
+button2.onclick = () => {
+    alert('Функция 2 пока в разработке!')
+}
